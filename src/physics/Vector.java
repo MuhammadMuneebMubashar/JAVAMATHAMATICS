bf494948144f7b4fcd02fafb076e947edcc54fa1
MuhammadMuneebMubashar <muhammadmuneebmubashar@gmail.com>
2026-03-03 00:51:21 +0500

    modified physics/vector class to use physics/ Coordinate class

--- src/physics/Vector.java
+++ src/physics/Vector.java
@@ -2,73 +2,73 @@ package physics;
 
 public class Vector {
 
-    private double x;
-    private double y;
-    private double z;
+    private Coordinate coordinate;
     private static final double EPS = 1e-9;
 
     public Vector(double x, double y)
     {
-        this.x = x;
-        this.y = y;
+        coordinate = new Coordinate(x, y);
     }
 
     public  Vector(double x, double y, double z)
     {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        coordinate = new Coordinate(x, y);
     }
 
     public Vector (double x1 , double x2 , double y1 , double y2)
     {
-        this.x = x2 - x1;
-        this.y = y2 - y1;
+        coordinate = new Coordinate(x2 - x1, y2 - y1);
     }
 
     public Vector (double x1 , double x2 , double y1 , double y2, double z1, double z2)
     {
-        this.x = x2 - x1;
-        this.y = y2 - y1;
-        this.z = z2 - z1;
+        coordinate = new Coordinate(x2 - x1, y2 - y1,  z2 - z1);
     }
 
     public double getX()
     {
-        return x;
+        return coordinate.getX();
     }
 
     public double getY()
     {
-        return y;
+        return coordinate.getY();
     }
 
     public double getZ()
     {
-        return z;
+        return coordinate.getZ();
     }
 
     public  Vector add(Vector vector2 )
     {
-        return new Vector(this.x + vector2.getX(),
-                this.y + vector2.getY(),
-                this.z + vector2.getZ());
+        return new Vector(this.getX() + vector2.getX(),
+                this.getY() + vector2.getY(),
+                this.getZ() + vector2.getZ());
     }
 
     public Vector sub(Vector vector2 )
     {
-        return new Vector(this.x - vector2.getX(),
-                this.y - vector2.getY(),
-                this.z - vector2.getZ());
+        return new Vector(this.getX() - vector2.getX(),
+                this.getY() - vector2.getY(),
+                this.getZ() - vector2.getZ());
     }
 
     public double magnitude()
     {
+        double x = this.getX();
+        double y = this.getY();
+        double z = this.getZ();
+
         return Math.sqrt(x*x + y*y + z*z);
     }
 
     public double dotProduct(Vector vector2 )
     {
+        double x = this.getX();
+        double y = this.getY();
+        double z = this.getZ();
+
         return x * vector2.getX()
                 + y * vector2.getY() +
                 z * vector2.getZ();
@@ -82,6 +82,10 @@ public class Vector {
     @Override
     public String toString()
     {
+        double x = this.getX();
+        double y = this.getY();
+        double z = this.getZ();
+
         String jSign = (y < 0)?"" : "+";
         String kSign = (z < 0)?"" : "+";
         return String.format("%.2fî  %s%.2fĵ  %s%.2fk̂\n",x, jSign,  y, kSign, z);
@@ -89,20 +93,24 @@ public class Vector {
 
     public void printCompForm()
     {
+        double x = this.getX();
+        double y = this.getY();
+        double z = this.getZ();
+
         System.out.printf("<%.2f  , %.2f  , %.2f>\n", x, y, z);
     }
 
     public boolean isEqual(Vector v2) {
-        return Math.abs(this.x - v2.x) < EPS &&
-                Math.abs(this.y - v2.y) < EPS &&
-                Math.abs(this.z - v2.z) < EPS;
+        return Math.abs(this.getX() - v2.getX()) < EPS &&
+                Math.abs(this.getY() - v2.getY()) < EPS &&
+                Math.abs(this.getZ() - v2.getZ()) < EPS;
     }
 
     public Vector crossProduct(Vector v2)
     {
-        double xPrime = this.y * v2.z - this.z * v2.y;
-        double yPrime = this.z * v2.x - this.x * v2.z;
-        double zPrime = this.x * v2.y - this.y * v2.x;
+        double xPrime = this.getY() * v2.getZ() - this.getZ() * v2.getY();
+        double yPrime = this.getZ() * v2.getX() - this.getX() * v2.getZ();
+        double zPrime = this.getX() * v2.getY() - this.getY() * v2.getX();
         return new Vector(xPrime, yPrime, zPrime);
     }
 
@@ -113,7 +121,7 @@ public class Vector {
 
     public Vector scale(double num)
     {
-        return new Vector(this.x * num, this.y * num, this.z * num);
+        return new Vector(this.getX() * num, this.getY() * num, this.getZ() * num);
     }
 
     public double angleBetween(Vector v2)
@@ -140,6 +148,6 @@ public class Vector {
         if (length < EPS){
             throw new ArithmeticException("Vector length zero");
         }
-        return new Vector(x/length, y/length, z/length);
+        return new Vector(this.getX()/length, this.getY()/length, this.getZ()/length);
     }
 }
